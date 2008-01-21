01685df28957c21e7381b180611caa339837cf90
Roger Ineichen <roger@projekt01.ch>
2008-01-21 09:01:24 +0000

    Added dev marker

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.language.switch',
-    version='1.0.0',
+    version='1.0.1dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Zope3 i18n language switch.",
