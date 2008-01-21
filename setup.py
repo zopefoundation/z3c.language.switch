af49ee8ea78f96b1abf734e6b4695335230c801e
Roger Ineichen <roger@projekt01.ch>
2008-01-21 08:57:04 +0000

    set version to 1.0.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.language.switch',
-    version='0.5.0',
+    version='1.0.0',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "Zope3 i18n language switch.",
