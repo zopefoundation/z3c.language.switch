f4b7ffd67770efc0449488e2f662c3e79fd3f94d
Stephan Richter <stephan.richter@gmail.com>
2008-01-25 06:08:52 +0000

    version bump.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.language.switch',
-    version='1.0.1',
+    version='1.0.2dev',
     author = "Roger Ineichen and the Zope Community",
     author_email = "zope-dev@zope.org",
     description = "Zope3 i18n language switch.",
